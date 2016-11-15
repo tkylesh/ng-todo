4c3f50af761952bc388f35112b6caab3e2e23998
Taylor Shuff <tkylesh@gmail.com>
2016-11-14 21:08:01 -0600

    day1-setup 11-14-2016

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,5 +1,26 @@
 "use strict";
 
-console.log("inside app.js")
+console.log("inside app.js");
+
+var app = angular.module("TodoApp",[]);
+
+app.controller("NavCtrl", function($scope){
+	$scope.navItems = [{name:"Logout"},{name:"All Items"},{name:"New Item"}];
+})
+
+app.controller("TodoCtrl",function($scope){
+	$scope.welcome = "hellow";
+	$scope.showListView= true;
+
+	$scope.allItems=function(){
+		console.log("you clicked all items");
+		$scope.showListView = true;
+	};
+
+	$scope.newItem=function(){
+		console.log("you clicked new item");
+		$scope.showListView = false;
+	};
+})
 
 
