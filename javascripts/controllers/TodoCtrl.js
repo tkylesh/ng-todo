36ab07d055d1ffecf69f5c9b011c33e38c12c18e
Taylor Shuff <tkylesh@gmail.com>
2016-11-15 21:03:10 -0600

    loaded items from firebase to dom.

--- javascripts/controllers/TodoCtrl.js
+++ javascripts/controllers/TodoCtrl.js
@@ -1,29 +1,14 @@
 "use strict";
 
-app.controller("TodoCtrl",function($scope){
+app.controller("TodoCtrl",function($scope, ItemFactory){
 	$scope.welcome = "hellow";
 	$scope.showListView= true;
 	$scope.newTask={};
-	$scope.items=[
-	{
-		id:0,
-		task:"mow the lawn",
-		isCompleted: true,
-		assignedTo: "Zoe"
-	},
-	{
-		id:1,
-		task:"grade quizzes",
-		isCompleted: false,
-		assignedTo: "William"
-	},
-	{
-		id:2,
-		task:"take a nap",
-		isCompleted: false,
-		assignedTo: "Zoe"
-	}
-	];
+	$scope.items=[];
+
+	ItemFactory.getItemList().then(function(fbItems){
+		$scope.items = fbItems;
+	});
 
 	$scope.allItems=function(){
 		console.log("you clicked all items");
