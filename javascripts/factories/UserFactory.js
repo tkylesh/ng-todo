d303d339708dd11f2336d03c17c262a81eff921e
Taylor Shuff <tkylesh@gmail.com>
2016-11-23 09:28:37 -0600

    corrected a typo. project is completed and ready to merge.

--- javascripts/factories/UserFactory.js
+++ javascripts/factories/UserFactory.js
@@ -19,7 +19,7 @@ app.factory("UserFactory", function($q, $http, FIREBASE_CONFIG){
 		});
 	};
 
-	let getUser = (userid) => {
+	let getUser = (userId) => {
 		return $q((resolve,reject) => {
 			$http.get(`${FIREBASE_CONFIG.databaseURL}/users.json?orderBy="uid"&equalTo="${userId}"`)
 			.success(function(userObject){
